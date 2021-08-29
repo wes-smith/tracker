3c7dacfafcffe771ca291667ef1a263b821d3669
Aidan <agoldfa7@u.rochester.edu>
2021-08-29 16:19:21 -0400

    commented and cleaned

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,92 @@
 use std::collections::HashMap;
 
-pub fn reuse_interval(trace: Vec<&str>) -> (Vec<usize>, HashMap<usize, f32>) {
-    let mut map = HashMap::new();
-    let mut res = Vec::new();
-    let mut calc = Vec::new();
+pub fn reuse_interval(trace: Vec<&str>) -> HashMap<usize, f32> {
+    /*
+        Init data structures
+    */
+    let mut im = HashMap::new(); // index map. k: trace element(address), v: last index element was read
+    let mut rdd = Vec::new(); //reuse distance distribution
+    let mut rrdd = Vec::new(); //real reuse distance distribution (rrd without inf.)
 
+    let mut freq_map = HashMap::new(); //frequence map. k: reuse distance, v: frequence
+    let mut dist_map = HashMap::new(); //distribution map. k: reuse distance, v: relative frequency
+                                       /*
+                                           End init data structures
+                                       */
+
+    /*
+        (Real)Reuse distance distribution (rdd) calculation
+    */
     for (i, val) in trace.iter().enumerate() {
-        println!("index: {}  value: {}", i, val);
-        if !map.contains_key(val) {
-            map.insert(val, i);
-            res.push(0);
+        if !im.contains_key(val) {
+            //first time seeing element, 0 instead of inf.
+            im.insert(val, i);
+            rdd.push(0);
         } else {
-            let dist = i - map[val];
-            res.push(dist);
-            calc.push(dist);
-            map.insert(val, i);
+            //revisiting element, need to update last index seen (equiv to caching element)
+            let dist = i - im[val];
+            rdd.push(dist);
+            rrdd.push(dist);
+            im.insert(val, i);
         }
     }
+    /*
+        End rdd calculation
+    */
 
-    let denom = calc.len();
-    let mut freq_map = HashMap::new(); //value,frequency map
-    for i in calc {
+    let denom = rrdd.len(); //denominator for relative frequency calculation. Number of non inf. reuse distances
 
-        //Something like this may be better
+    /*
+        Begin distance frequency calc. May be able to be done in-place
+    */
+    for i in rrdd {
+        //Something like this may be better (as clippy suggests)
         // match freq_map.entry(&i){
         //     Some(v) => freq_map.insert(&i, freq_map[&i] + 1),
         //     None => freq_map.insert(&i, 1),
         // }
-        
+
         if !freq_map.contains_key(&i) {
             freq_map.insert(i, 1);
         } else {
             freq_map.insert(i, freq_map[&i] + 1);
         }
     }
+    /*
+        End distance freq calc
+    */
 
-    let mut dist_map = HashMap::new();
+    /*
+        Begin relative distribution frequency calculation
+    */
     for (k, v) in freq_map.iter() {
         let freq: f32 = *v as f32 / denom as f32;
         dist_map.insert(*k, freq);
     }
+    /*
+        End relative distribution frequeny calculation
+    */
 
-    (res, dist_map)
+    /*
+        Return the relative distribution
+    */
+    dist_map
+    /*
+        End function
+    */
 }
+
+/// Insertion sort from https://chercher.tech/rust/insertion-sort-rust
+
+// pub fn insertion_sort(arr: &mut [i32]) {
+//     for i in 1..arr.len() {
+//         let mut j = i;
+//         while j > 0 && arr[j - 1] > arr[j] {
+//             arr.swap(j - 1, j);
+//             j -= 1;
+//         }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -57,9 +101,7 @@ mod tests {
     #[test]
     fn ri_cyclic() {
         let arr = vec!["a", "b", "c", "a", "b", "c"];
-        let (res, dist) = reuse_interval(arr);
-
-        assert_eq!(res, vec![0, 0, 0, 3, 3, 3]);
+        let dist = reuse_interval(arr);
 
         let mut map = HashMap::new();
         map.insert(3, 1.0);
@@ -70,9 +112,7 @@ mod tests {
     #[test]
     fn ri_sawtooth() {
         let arr = vec!["a", "b", "c", "c", "b", "a"];
-        let (res, dist) = reuse_interval(arr);
-
-        assert_eq!(res, vec![0, 0, 0, 1, 3, 5]);
+        let dist = reuse_interval(arr);
 
         let mut map = HashMap::new();
         map.insert(1, 0.33333334);
@@ -81,4 +121,13 @@ mod tests {
 
         assert_eq!(dist, map);
     }
+
+    // #[test]
+    // fn insert_sort() {
+    //     let mut arr = vec![5,4,3,2,1];
+    //     let sorted = vec![1,2,3,4,5];
+    //     insertion_sort(&mut arr);
+
+    //     assert_eq!(arr,sorted);
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,9 @@
-use log::{trace, LevelFilter, SetLoggerError};
-use log4rs::{
-    append::file::FileAppender,
-    config::{Appender, Config, Root},
-    encode::pattern::PatternEncoder,
-};
 mod lib;
 
-fn main() -> Result<(), SetLoggerError> {
-    let file_path = "foo.log";
-
-    let logfile = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new("{m}\n")))
-        .build(file_path)
-        .unwrap();
-
-    let config = Config::builder()
-        .appender(Appender::builder().build("trace", Box::new(logfile)))
-        .build(Root::builder().appender("trace").build(LevelFilter::Trace))
-        .unwrap();
-
-    let _handle = log4rs::init_config(config)?;
-
-    let arr = vec![0,1,2,3,4,5,6,7,8,9];
-    let mut _sum = 0;
-    for item in arr.iter() {
-        _sum += item;
-        trace!("sum: {:?} ", _sum);
-    }
-
-
-    let arr2 = vec!["a", "b", "c", "a", "b", "c"];
-    let (res, _map) = lib::reuse_interval(arr2);
-    println!("{:?}", res);
-
-    Ok(())
+fn main() {
+    let arr = vec!["a", "b", "c", "a", "b", "c"];
+    // let arr = vec![
+    //     "a", "b", "a", "b", "a", "b", "c", "c", "c", "c", "c", "c", "a",
+    // ];
+    let _map = lib::reuse_interval(arr);
 }
