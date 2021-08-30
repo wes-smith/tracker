ed7ca7d400abb4dab2471d54d56f8554f1145e14
Aidan <agoldfa7@u.rochester.edu>
2021-08-30 16:32:19 -0400

    naive reuse distance

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,112 @@ pub fn reuse_interval(trace: Vec<&str>) -> HashMap<usize, f32> {
         Init data structures
     */
     let mut im = HashMap::new(); // index map. k: trace element(address), v: last index element was read
-    let mut rdd = Vec::new(); //reuse distance distribution
-    let mut rrdd = Vec::new(); //real reuse distance distribution (rrd without inf.)
+    let mut rid = Vec::new(); //reuse interval distribution
+    let mut rrid = Vec::new(); //real reuse interval distribution (rrd without inf.)
 
-    let mut freq_map = HashMap::new(); //frequence map. k: reuse distance, v: frequence
-    let mut dist_map = HashMap::new(); //distribution map. k: reuse distance, v: relative frequency
+    let mut freq_map = HashMap::new(); //frequence map. k: reuse interval, v: frequence
+    let mut dist_map = HashMap::new(); //distribution map. k: reuse interval, v: relative frequency
     /*
         End init data structures
     */
 
     /*
-        (Real)Reuse distance distribution (rdd) calculation
+        (Real)Reuse interval distribution (rdd) calculation
+    */
+    for (i, val) in trace.iter().enumerate() {
+        if !im.contains_key(val) {
+            //first time seeing element, 0 instead of inf.
+            im.insert(val, i);
+            rid.push(0);
+        } else {
+            //revisiting element, need to update last index seen (equiv to caching element)
+            let dist = i - im[val];
+            rid.push(dist);
+            rrid.push(dist);
+            im.insert(val, i);
+        }
+    }
+    /*
+        End rdd calculation
+    */
+
+    let denom = rrid.len(); //denominator for relative frequency calculation. Number of non inf. reuse distances
+
+    /*
+        Begin distance frequency calc. May be able to be done in-place
+    */
+    for i in rrid {
+        //Something like this may be better (as clippy suggests)
+        // match freq_map.entry(&i){
+        //     Some(v) => freq_map.insert(&i, freq_map[&i] + 1),
+        //     None => freq_map.insert(&i, 1),
+        // }
+
+        if !freq_map.contains_key(&i) {
+            freq_map.insert(i, 1);
+        } else {
+            freq_map.insert(i, freq_map[&i] + 1);
+        }
+    }
+    /*
+        End distance freq calc
+    */
+
+    /*
+        Begin relative distribution frequency calculation
+    */
+    for (k, v) in freq_map.iter() {
+        let freq: f32 = *v as f32 / denom as f32;
+        dist_map.insert(*k, freq);
+    }
+    /*
+        End relative distribution frequeny calculation
+    */
+
+    /*
+        Return the relative distribution
+    */
+    dist_map
+    /*
+        End function
+    */
+}
+
+
+fn calc_distance(window: Vec<&str>) -> usize{
+    let mut res: usize = 1;
+    let mut last = window.first().unwrap();
+    for val in window.iter(){
+        if val == last{
+            continue;
+        }
+        else{
+            res = res + 1;
+            last = val;
+        }
+    }
+    res
+}
+
+pub fn reuse_distance(trace: Vec<&str>)-> HashMap<usize, f32> {
+     /*
+        Init data structures
+    */
+    let mut im = HashMap::new(); // index map. k: trace element(address), v: last index element was read
+    let mut rdd = Vec::new(); //reuse interval distribution
+    let mut rrdd = Vec::new(); //real reuse interval distribution (rrd without inf.)
+
+    let mut freq_map = HashMap::new(); //frequence map. k: reuse interval, v: frequence
+    let mut dist_map = HashMap::new(); //distribution map. k: reuse interval, v: relative frequency
+
+    // let mut freq_map = HashMap::new(); //frequence map. k: reuse interval, v: frequence
+    // let mut dist_map = HashMap::new(); //distribution map. k: reuse interval, v: relative frequency
+    /*
+        End init data structures
+    */
+
+    /*
+        (Real)Reuse interval distribution (rdd) calculation
     */
     for (i, val) in trace.iter().enumerate() {
         if !im.contains_key(val) {
@@ -23,8 +118,8 @@ pub fn reuse_interval(trace: Vec<&str>) -> HashMap<usize, f32> {
             im.insert(val, i);
             rdd.push(0);
         } else {
-            //revisiting element, need to update last index seen (equiv to caching element)
-            let dist = i - im[val];
+            let left = im[val];
+            let dist = calc_distance(trace[left..i].to_vec());
             rdd.push(dist);
             rrdd.push(dist);
             im.insert(val, i);
@@ -33,7 +128,6 @@ pub fn reuse_interval(trace: Vec<&str>) -> HashMap<usize, f32> {
     /*
         End rdd calculation
     */
-
     let denom = rrdd.len(); //denominator for relative frequency calculation. Number of non inf. reuse distances
 
     /*
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod lib;
 
 fn main() {
-    let arr = vec!["a", "b", "c", "a", "b", "c"];
+    // let arr = vec!["a", "b", "c", "a", "b", "c"];
     // let arr = vec![
     //     "a", "b", "a", "b", "a", "b", "c", "c", "c", "c", "c", "c", "a",
     // ];
-    let _map = lib::reuse_interval(arr);
+    // let _map = lib::reuse_interval(arr);
+    let arr = vec!["a","b","b","b","c","b","b","a"];
+    let _res = lib::reuse_distance(arr);
+    println!("{:?}", _res);
 }
