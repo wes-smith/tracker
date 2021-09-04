ea61f6cd62d0ade90216da51328f2666cc825e9d
Aidan Goldfarb <agoldfa7@tabletop.csug.rochester.edu>
2021-09-04 17:12:42 -0400

    time to graph

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,15 @@ fn main() -> std::io::Result<()>{
     let size = args[1].parse::<usize>().unwrap();
     let mut reps = args[2].parse::<usize>().unwrap();
 
-    let mut output = File::create("output.txt").unwrap();
+    let file_name = String::from("s".to_string() + &args[1] + &".txt".to_string() );
+    let mut output = File::create(file_name).unwrap();
 
     let mut rng = WyRand::new();
     let mut arr = init_arr(size);
     let mut avg: f32 = 0.0;
     let mut iteration = 1;
     while reps > 1 {
-        quick_sort(&mut arr, "smoke");
+        quick_sort(&mut arr, "foo.log");
         rng.shuffle(&mut arr);
         let trace: Vec<String> = parse::build_trace("foo.log").unwrap();
         let trace_ref: Vec<&str> = trace.iter().map(AsRef::as_ref).collect();
